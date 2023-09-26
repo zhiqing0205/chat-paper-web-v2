0cf84a3ef76beb3a41e481c4dd7e496102c3b3ea
Zhiqing Zhong <1775840762@qq.com>
2023-09-27 05:06:38 +0800

    导航去除反馈和国际化增加通知模块

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -1,10 +1,10 @@
-import { QuestionCircleOutlined } from '@ant-design/icons';
-import { Space } from 'antd';
+import { message, Space } from 'antd';
 import React from 'react';
-import { SelectLang, useModel } from 'umi';
+import { useModel } from 'umi';
 import HeaderSearch from '../HeaderSearch';
 import Avatar from './AvatarDropdown';
 import styles from './index.less';
+import NoticeIcon from '@/components/NoticeIcon/NoticeIcon';
 
 export type SiderTheme = 'light' | 'dark';
 
@@ -21,6 +21,24 @@ const GlobalHeaderRight: React.FC = () => {
 	if ((navTheme === 'dark' && layout === 'top') || layout === 'mix') {
 		className = `${styles.right}  ${styles.dark}`;
 	}
+
+	const list = [
+		{
+			id: '000000001',
+			avatar: 'https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png',
+			title: '你收到了 14 份新周报',
+			datetime: '2017-08-09',
+			type: 'notification',
+		},
+		{
+			id: '000000002',
+			avatar: 'https://gw.alipayobjects.com/zos/rmsportal/OKJXDXrmkNshAMvwtvhu.png',
+			title: '你推荐的 曲妮妮 已通过第三轮面试',
+			datetime: '2017-08-08',
+			type: 'notification',
+		},
+	];
+
 	return (
 		<Space className={className}>
 			<HeaderSearch
@@ -49,16 +67,53 @@ const GlobalHeaderRight: React.FC = () => {
 				//   console.log('input', value);
 				// }}
 			/>
-			<span
-				className={styles.action}
-				onClick={() => {
-					window.open('https://pro.ant.design/docs/getting-started');
+			{/*<span*/}
+			{/*	className={styles.action}*/}
+			{/*	onClick={() => {*/}
+			{/*		window.open('https://pro.ant.design/docs/getting-started');*/}
+			{/*	}}*/}
+			{/*>*/}
+			{/*	<QuestionCircleOutlined />*/}
+			{/*</span>*/}
+			<NoticeIcon
+				count={10}
+				onItemClick={(item) => {
+					message.info(`${item.title} 被点击了`);
 				}}
+				onClear={(title: string, key: string) => message.info('点击了清空更多')}
+				loading={false}
+				clearText="清空"
+				viewMoreText="查看更多"
+				onViewMore={() => message.info('点击了查看更多')}
+				clearClose
 			>
-				<QuestionCircleOutlined />
-			</span>
+				<NoticeIcon.Tab
+					tabKey="notification"
+					count={2}
+					list={list}
+					title="通知"
+					emptyText="你已查看所有通知"
+					showViewMore
+				/>
+				<NoticeIcon.Tab
+					tabKey="message"
+					count={2}
+					list={list}
+					title="消息"
+					emptyText="您已读完所有消息"
+					showViewMore
+				/>
+				<NoticeIcon.Tab
+					tabKey="event"
+					title="待办"
+					emptyText="你已完成所有待办"
+					count={2}
+					list={list}
+					showViewMore
+				/>
+			</NoticeIcon>
 			<Avatar />
-			<SelectLang className={styles.action} />
+			{/*<SelectLang className={styles.action} />*/}
 		</Space>
 	);
 };
